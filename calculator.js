840986c39d6501831c33f8e04368b4442503c3e3
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-17 09:20:41 +0200

    feat: implement function to multiply two values
    Function returns the product of two values, if it's arguments are of
    the number type.

--- calculator.js
+++ calculator.js
@@ -9,7 +9,15 @@ const add = function(a, b) {
 const subtract = function(a, b) {
   if (isNaN(a) || isNaN(b)) { 
     throw new Error('Can\'t subtract these two values from each other, check your input.');
-  } // Check for values that aren't numbers, accepts numbers as strings, but not strings containing non-numbers
+  }
 
-  return Number(a) - Number(b); // Make sure the numbers entered are of the 'Number' type
+  return Number(a) - Number(b);
+};
+
+const multiply = function(a, b) {
+  if (isNaN(a) || isNaN(b)) { 
+    throw new Error('Can\'t calculate the product of these two values, check your input.');
+  }
+
+  return Number(a) * Number(b);
 }
