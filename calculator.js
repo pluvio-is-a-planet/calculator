e42caf38b8e630ce1e98d1d2cc17004b744fde58
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-17 09:16:55 +0200

    feat: implement function to subtract two values
    Function takes two arguments, a and b, and returns the difference
    between them.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
 const add = function(a, b) {
-
   if (isNaN(a) || isNaN(b)) { 
     throw new Error('Can\'t add these two values together, check your input.');
   } // Check for values that aren't numbers, accepts numbers as strings, but not strings containing non-numbers
 
   return Number(a) + Number(b); // Make sure the numbers entered are of the 'Number' type
 };
+
+const subtract = function(a, b) {
+  if (isNaN(a) || isNaN(b)) { 
+    throw new Error('Can\'t subtract these two values from each other, check your input.');
+  } // Check for values that aren't numbers, accepts numbers as strings, but not strings containing non-numbers
+
+  return Number(a) - Number(b); // Make sure the numbers entered are of the 'Number' type
+}
