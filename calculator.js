eedd4ce542667382447fc5f5a572c0661046d18e
pluvioisaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-07-29 16:30:04 +0200

    Add input validation to calculator functions

--- calculator.js
+++ calculator.js
@@ -1,17 +1,57 @@
 const calculator = new Object();
 
 calculator.add = function(a, b) {
-    return a + b;
+    let validated = [];
+
+    try {
+        validated = validateInput(a, b);
+    } catch (error) {
+        return error;
+    }
+
+    return validated[0] + validated[1];
 };
 
 calculator.subtract = function(a, b) {
-    return a - b;
+    let validated = [];
+
+    try { 
+        validated = validateInput(a, b);
+    } catch (error) {
+        return error;
+    }
+
+    return validated[0] - validated[1];
 };
 
 calculator.multiply = function(a, b) {
-    return a * b;
+    let validated = [];
+
+    try {
+        validated = validateInput(a, b);
+    } catch (error) {
+        return error;
+    }
+
+    return validated[0] * validated[1];
 };
 
 calculator.divide = function(a, b) {
-    return a / b;
+    let validated = [];
+
+    try {
+        validated = validateInput(a, b);
+    } catch (error) {
+        return error;
+    }
+
+    return validated[0] / validated[1];
 };
+
+function validateInput(a, b) {
+    if (isNaN(a) || isNaN(b)) { 
+        throw new Error("Not a valid number.");
+    }
+
+    return [a, b];
+}
