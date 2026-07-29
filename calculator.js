4e9a77b6f599ab4efa9d7f00c5a3306f3f856ae9
pluvioisaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-07-29 16:06:30 +0200

    Add calculator object and basic functionality

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const calculator = new Object();
+
+calculator.add = function(a, b) {
+    return a + b;
+};
+
+calculator.subtract = function(a, b) {
+    return a - b;
+};
+
+calculator.multiply = function(a, b) {
+    return a * b;
+};
+
+calculator.divide = function(a, b) {
+    return a / b;
+};
